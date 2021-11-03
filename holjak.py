7840c50b15d42ea0ec7d71629b6728ddf8d20056
minkyu2346 <kmk2346@naver.com>
2021-11-03 15:33:03 +0900

    홀짝 게임 개발

--- holjak.py
+++ holjak.py
@@ -1,8 +1,10 @@
+import random
 print("오징어 게임에 오신 것을 환영합니다")
 print("홀짝 게임을 시작합니다")
 
 while True:
-    a= int(input());
+    a= random.randint(1,10)
+    print(a)
     my=str(input("홀수 또는 짝수를 입력하세요 "))
     if a%2 == 0:
         print("구슬은 짝수 입니다")
