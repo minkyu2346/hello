1ad8e1c0e7312eff85b3da5e0868f49ffa070912
minkyu2346 <kmk2346@naver.com>
2021-11-04 11:40:20 +0900

    배팅 조건 추가

--- loop.py
+++ loop.py
@@ -8,13 +8,13 @@ print("홀짝 게임을 시작합니다")
 while i<2:
     a= random.randint(1,10)
     print("당신의 구슬의 갯수: {}개".format(marble))
-    bet= input("구슬 몇개 배팅?")
+    bet= input("구슬 몇개 배팅? (잘못 입력하면 한번은 기회 줄게)")
     if int(bet)>marble:
         i=i+1 
-        print("배팅갯수 초과 다시 입력")
+        print("배팅갯수 초과 다시 입력 ")
         continue
     
-    
+
     my= input("홀 짝? (잘못 입력하면 한번은 기회 줄게)")
     print(my)
     if my != "홀" and my != "짝": 
@@ -36,7 +36,9 @@ while i<2:
              print("맞다 ")
              marble= marble+int(bet)
              print("당신의 구슬의 갯수 :{}개".format(marble))
-            
+             if marble>=20 :
+                 print("이겼다 다음 스테이지")
+                 break
 
         else:
              print("틀렸다 구슬 뺏김") 
